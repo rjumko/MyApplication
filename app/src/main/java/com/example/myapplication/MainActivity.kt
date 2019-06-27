972c9393daad48ef073cb98ec94a0bbc54666b0d
rjumko <rjumko@gmail.com>
2019-06-27 12:08:31 +0700

    Add onClick fun button 'Toast'

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        val toast = Toast.makeText(this, "Toast Me!!!", Toast.LENGTH_SHORT)
+
+        toast.show()
     }
 }
